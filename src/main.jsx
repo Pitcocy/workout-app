ae6b5d73cd1061d0eb52530f1fcc08bf29190e7b
Pitcocy <alfred.botond@gmail.com>
2024-08-25 08:16:42 +0200

    added logging to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import './firebase'
+import * as firebase from './firebase'
+
+console.log('Firebase imported in main.jsx:', firebase)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
